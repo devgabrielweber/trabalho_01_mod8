d75680805ca33893ec39550f675c099d9d1b6d20
jackson <lordjackson@gmail.com>
2023-09-13 14:12:02 -0300

    atualizando a rota para a versão CRUD aluno

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+//importar o arquivo do controlador
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\AlunoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +24,51 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+//chamar uma função do controlador
+Route::get('/usuario', [UsuarioController::class, 'index']);
+
+//carrega uma listagem do banco
+Route::get('/aluno',
+    [AlunoController::class, 'index'])->name('aluno.index');
+
+//chama o formulário do aluno
+Route::get('/aluno/create',
+    [AlunoController::class, 'create'])->name('aluno.create');
+
+ //realiza a ação de salvar do formulário
+ Route::post('/aluno',
+    [AlunoController::class, 'store'])->name('aluno.store');
+
+//chama o formulário para edição
+Route::get('/aluno/edit/{id}',
+    [AlunoController::class, 'edit'])->name('aluno.edit');
+
+ //realiza a ação de atualizar os dados do formulário
+ Route::put('/aluno/update/{id}',
+    [AlunoController::class, 'update'])->name('aluno.update');
+
+//chama o método para excluir o registro
+Route::get('/aluno/destroy/{id}',
+    [AlunoController::class, 'destroy'])->name('aluno.destroy');
+
+//chama o método para serch para pesquisar e filtrar o registro da listagem
+Route::post('/aluno/search',
+    [AlunoController::class, 'search'])->name('aluno.search');
+
+//chamar uma página em HTML
+Route::get('/pagina', function () {
+    return view('index');
+});
+
+//chama um HTML
+Route::get('/teste', function () {
+    return "<h4>Olá Mundo Laravel!</h4>";
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
